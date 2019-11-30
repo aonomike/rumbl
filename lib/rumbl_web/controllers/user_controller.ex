fb4b8d61809dce101666ee50a11588380e5153d2
aonomike <aonomike@gmail.com>
2019-11-30 18:04:56 +0300

    Implement authentication plug

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,35 +1,50 @@
 defmodule RumblWeb.UserController do
-    @moduledoc """
-    User Controller
-    """
-    use RumblWeb, :controller
+  @moduledoc """
+  User Controller
+  """
+  use RumblWeb, :controller
 
-    alias Rumbl.Accounts
-    alias Rumbl.Accounts.User
+  alias Rumbl.Accounts
+  alias Rumbl.Accounts.User
 
-    def index(conn, _params) do
-        users = Accounts.list_users()
-        render(conn, "index.html", users: users)
-    end
+  plug :authenticate when action in [:index, :show]
 
-    def show(conn, %{"id"=> id}) do
-        user = Accounts.get_user(id)
-        render(conn, "show.html", user: user)
-    end
+  def index(conn, _params) do
+    users = Accounts.list_users()
+    render(conn, "index.html", users: users)
+  end
 
-    def new(conn, _params) do
-        changeset = Accounts.change_registration(%User{}, %{})
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user(id)
+    render(conn, "show.html", user: user)
+  end
+
+  def new(conn, _params) do
+    changeset = Accounts.change_registration(%User{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.register_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> RumblWeb.Auth.login(user)
+        |> put_flash(:info, "#{user.name} created")
+        |> redirect(to: Routes.user_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
+  end
 
-    def create(conn, %{"user" => user_params}) do
-      case Accounts.register_user(user_params) do
-        {:ok, user} ->
-          conn
-            |> put_flash(:info, "#{user.name} created")
-            |> redirect(to: Routes.user_path(conn, :index))
-        {:error, %Ecto.Changeset{} = changeset } ->
-          render(conn, "new.html", changeset: changeset)
-      end 
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "you must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
     end
+  end
 end
